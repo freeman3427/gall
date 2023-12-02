acd0f8bdd32c00769342db9285006f3259f2cafc
freeman3427 <lyc3427>
2023-12-02 13:06:29 +0900

    data

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Gallery(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
