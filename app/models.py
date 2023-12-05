cc81a06b04962eda5ac02c1a6c25aaa7fb9b64e9
freeman3427 <lyc3427>
2023-12-06 00:50:01 +0900

    12/06

--- app/models.py
+++ app/models.py
@@ -15,3 +15,10 @@ class Comment(db.Model):
     gall = db.relationship('Gallery', backref=db.backref('comment_set'))
     content = db.Column(db.Text, nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
